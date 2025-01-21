ee3a4114f4b7c437cb7480b7757932ef98bbe451
Joseph11024 <joseph.olsen1989@gmail.com>
2025-01-21 14:23:42 -0600

    Attempt to remove "Found Link Trade Partner"  duplicate messages.

--- SysBot.Pokemon/SV/BotTrade/PokeTradeBotSV.cs
+++ SysBot.Pokemon/SV/BotTrade/PokeTradeBotSV.cs
@@ -1236,8 +1236,6 @@ public class PokeTradeBotSV(PokeTradeHub<PK9> Hub, PokeBotState Config) : PokeRo
             return PokeTradeResult.TrainerTooSlow;
         }
 
-        poke.SendNotification(this, $"Found Link Trade partner: {tradePartner.TrainerName}. TID: {tradePartner.TID7} SID: {tradePartner.SID7} Waiting for a Pokémon...");
-
         if (poke.Type == PokeTradeType.Dump)
         {
             var result = await ProcessDumpTradeAsync(poke, token).ConfigureAwait(false);
